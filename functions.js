61b1ff2b5406b509aebd1fe6cd9b3dfc64ca4f79
Javier Olivieri <javierolivieri3@gmail.comm>
2023-06-02 21:49:53 -0300

    Version Final

--- functions.js
+++ functions.js
@@ -16,17 +16,3 @@ links.forEach(link => {
 function redireccionar(url) {
     window.open(url, "_blank");
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    var inputs = document.querySelectorAll('.js-input');
-  
-    for (var i = 0; i < inputs.length; i++) {
-      inputs[i].addEventListener('keyup', function() {
-        if (this.value) {
-          this.classList.add('not-empty');
-        } else {
-          this.classList.remove('not-empty');
-        }
-      });
-    }
-  });
